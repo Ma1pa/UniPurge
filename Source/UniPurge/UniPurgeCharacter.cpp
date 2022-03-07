da0ad321af950417828273c99bb7e80e6a8bf913
Ma1pa <ma1paespana@gmail.com>
2022-03-07 19:17:07 +0100

    Added controls and TODOs
    Added most of the in-game controls alongside with their associated functions and the TODOs of those functions

--- Source/UniPurge/UniPurgeCharacter.cpp
+++ Source/UniPurge/UniPurgeCharacter.cpp
@@ -45,6 +45,9 @@ AUniPurgeCharacter::AUniPurgeCharacter()
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
+
+	//Other Variables
+	IsRunning = false;
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -56,9 +59,14 @@ void AUniPurgeCharacter::SetupPlayerInputComponent(class UInputComponent* Player
 	check(PlayerInputComponent);
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+	PlayerInputComponent->BindAction("Run", IE_Pressed, this, &AUniPurgeCharacter::StartSprint);
+	PlayerInputComponent->BindAction("Run", IE_Released, this, &AUniPurgeCharacter::StopSprint);
+	PlayerInputComponent->BindAction("Hide", IE_Pressed, this, &AUniPurgeCharacter::StartHide);
+	PlayerInputComponent->BindAction("Hide", IE_Released, this, &AUniPurgeCharacter::StopHide);
 
 	PlayerInputComponent->BindAxis("MoveForward", this, &AUniPurgeCharacter::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &AUniPurgeCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("DimensionShift", this, &AUniPurgeCharacter::DimensionShift);
 
 	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
 	// "turn" handles devices that provide an absolute delta, such as a mouse.
@@ -67,6 +75,9 @@ void AUniPurgeCharacter::SetupPlayerInputComponent(class UInputComponent* Player
 	PlayerInputComponent->BindAxis("TurnRate", this, &AUniPurgeCharacter::TurnAtRate);
 	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 	PlayerInputComponent->BindAxis("LookUpRate", this, &AUniPurgeCharacter::LookUpAtRate);
+
+	// Set up menu key bindings
+	PlayerInputComponent->BindAction("Menu", IE_Pressed, this, &AUniPurgeCharacter::Menu);
 }
 
 void AUniPurgeCharacter::TurnAtRate(float Rate)
@@ -83,6 +94,8 @@ void AUniPurgeCharacter::LookUpAtRate(float Rate)
 
 void AUniPurgeCharacter::MoveForward(float Value)
 {
+	//TODO: If IsRunning -> Multiply Value by some amount (1.x)
+	//TODO: If IsHiding -> Multiply Value by some amount (0.x)
 	if ((Controller != nullptr) && (Value != 0.0f))
 	{
 		// find out which way is forward
@@ -97,6 +110,8 @@ void AUniPurgeCharacter::MoveForward(float Value)
 
 void AUniPurgeCharacter::MoveRight(float Value)
 {
+	//TODO: If IsRunning -> Multiply Value by some amount (1.x)
+	//TODO: If IsHiding -> Multiply Value by some amount (0.x)
 	if ( (Controller != nullptr) && (Value != 0.0f) )
 	{
 		// find out which way is right
@@ -108,4 +123,42 @@ void AUniPurgeCharacter::MoveRight(float Value)
 		// add movement in that direction
 		AddMovementInput(Direction, Value);
 	}
+}
+
+void AUniPurgeCharacter::DimensionShift(float Intensity)
+{
+	// TODO: Implementation of the Mechanic | Requires Implementation of the generation and modification systemsRequires Implementation of the generation and modification systems
+}
+
+void AUniPurgeCharacter::StartSprint()
+{
+	IsRunning = true;
+	// TODO: Change animation to sprinting animations
+	// TODO: Check for water movement
+}
+
+void AUniPurgeCharacter::StopSprint()
+{
+	IsRunning = false;
+	// TODO: Change animation to walking animations
+	// TODO: Check for water movement
+}
+
+void AUniPurgeCharacter::StartHide()
+{
+	//IsHiding = true;
+	// TODO: Check for "Hiding Spots"
+	// TODO: If no "Hiding Spots"; change animation to crouching animations
+}
+
+void AUniPurgeCharacter::StopHide()
+{
+	//IsHiding = false;
+	// TODO: Change animation to walking animations
+}
+
+void AUniPurgeCharacter::Menu()
+{
+	// TODO: If the InGame Menu is closed, Open the Menu
+	// TODO: If the InGame Menu is open, Close the Menu
 }
--- Source/UniPurge/UniPurgeCharacter.h
+++ Source/UniPurge/UniPurgeCharacter.h
@@ -11,30 +11,38 @@ class AUniPurgeCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
-	/** Camera boom positioning the camera behind the character */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	/* Camera boom positioning the camera behind the character */
 	class USpringArmComponent* CameraBoom;
 
-	/** Follow camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	/* Follow camera */
 	class UCameraComponent* FollowCamera;
+
 public:
 	AUniPurgeCharacter();
 
-	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	/* Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
 	float BaseTurnRate;
 
-	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	/* Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
 	float BaseLookUpRate;
+
+	/* Boolean indicating if the character is currently hiding*/
+	bool IsHiding;
+
+private:
+	/* Boolean indicating if the character is running */
+	bool IsRunning;
 
 protected:
 
-	/** Called for forwards/backward input */
+	/* Called for forwards/backward input */
 	void MoveForward(float Value);
 
-	/** Called for side to side input */
+	/* Called for side to side input */
 	void MoveRight(float Value);
 
 	/** 
@@ -49,6 +57,30 @@ protected:
 	 */
 	void LookUpAtRate(float Rate);
 
+	/**
+	* Called via input to change between world states and animations.
+	* @param Intensity	This is the intensity of the animation. Only at 1 or -1 does it change states
+	*/
+	void DimensionShift(float Intensity);
+
+	/* Called via input to indicate that the sprint action has started	*/
+	void StartSprint();
+
+	/* Called via input to indicate that the sprint action has ended */
+	void StopSprint();
+
+	/* Called via input to indicate that the hiding action has started */
+	void StartHide();
+
+	/* Called via input to indicate that the hiding action has ended */
+	void StopHide();
+
+	/* Called via input to indicate to toggle the Menu	*/
+	void Menu();
+
+	// TODO: Add Wall Climbing
+	// TODO: Add Swimming
+
 protected:
 	// APawn interface
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
@@ -59,5 +91,7 @@ public:
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 	/** Returns FollowCamera subobject **/
 	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+	// TODO: Add return of IsHiding (Not know if INLINE or not)
 };
 
